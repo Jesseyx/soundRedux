c7892ff705d1966fa00af496b49b59ac5ee40d1f
Jesseyx <jyaoxiao@hotmail.com>
2016-04-01 15:48:05 +0800

    add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    main: [
+
+    ]
+  },
+  output: {
+    publicPath: 'http://localhost:8080/',
+    filename: '/js/[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'autoprefixer', 'sass']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/\.svg$/)
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api/*': 'http://localhost:8081'
+    }
+  }
+}
