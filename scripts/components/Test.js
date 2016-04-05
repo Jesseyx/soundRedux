c4404623452ce2f6a6d5864f699b1b25928a7d8e
Jesseyx <jyaoxiao@hotmail.com>
2016-04-05 20:49:11 +0800

    Complete react-redux framework

---
+++ scripts/components/Test.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Test extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello, world!!!</h2>
+      </div>
+    )
+  }
+}
+
+Test.propsTypes = {
+
+}
---
+++ scripts/containers/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Test from '../components/Test';
+
+
+function mapStateToProps(props, ownProps) {
+  return {
+
+  }
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Test);
