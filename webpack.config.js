351bd95bf20b62ffad8afa096cce032f791cc6e8
Jesseyx <jyaoxiao@hotmail.com>
2016-09-22 20:32:49 +0800

    Do build...

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,37 @@
-var webpack = require('webpack')
+var webpack = require('webpack');
+var ignore = new webpack.IgnorePlugin(/\.svg$/);
 
 module.exports = {
     devtool: 'source-map',
     entry: {
         main: [
             './scripts/main.js'
-        ]
+        ],
     },
     output: {
         publicPath: 'http://localhost:8080/',
-        filename: '/js/[name].js'
+        filename: '/js/[name].js',
     },
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 loaders: ['react-hot', 'babel'],
-                exclude: /node_modules/
+                exclude: /node_modules/,
             },
             {
                 test: /\.scss$/,
-                loaders: ['style', 'css', 'autoprefixer', 'sass']
-            }
+                loaders: ['style', 'css', 'postcss', 'sass'],
+            },
         ]
     },
     plugins: [
-        new webpack.IgnorePlugin(/\.svg$/)
+        ignore,
     ],
     devServer: {
         host: '0.0.0.0',
         proxy: {
             '/api/*': 'http://localhost:8081'
-        }
-    }
+        },
+    },
 }
